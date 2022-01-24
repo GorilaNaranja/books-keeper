b99e6f7077edfa4b8585b5e65d2c8e3d9f83336f
Felipe <fcalcalx@gmail.com>
2022-01-24 15:22:11 +0100

    Update: author service constructor

--- src/main/java/com/feliopolis/bookskeeper/authors/AuthorService.java
+++ src/main/java/com/feliopolis/bookskeeper/authors/AuthorService.java
@@ -3,17 +3,20 @@ package com.feliopolis.bookskeeper.authors;
 import com.feliopolis.bookskeeper.users.User;
 import com.feliopolis.bookskeeper.users.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@RequiredArgsConstructor
 public class AuthorService {
 
     private final AuthorRepository authorRepository;
 
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
 
     /*@Transactional
     public UserDetails loadUserByEmail(String email) throws UsernameNotFoundException {
